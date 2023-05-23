b5d92c1476eecc617d7a32e687ff1d19520b3ec8
rrsrnc <rajeev7605@gmail.com>
2023-05-23 14:25:36 +0530

    token error handled

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -8,13 +8,16 @@ const fetchuser = (req, res, next) => {
   if (!token) {
       res.status(401).send({ error: "Please authenticate using a valid token" })
   }
-  try {
-      const data = jwt.verify(token, secretKey);
-      req.user = data.user;
-      next();
-  } catch (error) {
-      res.status(401).send({ error: "Please authenticate using a valid token" })
+  else{
+    try {
+        const data = jwt.verify(token, secretKey);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
   }
+  
 
 }
 
